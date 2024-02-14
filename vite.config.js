3f7a97e991be774363c88489dc51fce1684ddb98
apoorwagupta <apoorwa.gupta01@gmail.com>
2024-02-14 22:19:04 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<base href="/WeatherAppReact/" />
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/WeatherAppReact/"
 })
